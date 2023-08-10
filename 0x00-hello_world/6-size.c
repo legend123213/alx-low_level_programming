432ec69fb3152fb2d0c0c9168c7e59ad6cddd2ab
Abel Wendmu <abelwendmu124@gmail.com>
2023-08-10 12:04:35 -0700

    set up for c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: 0 on success
+ */
+int main(void)
+{
+	printf("Size of a char: %1 byte(s)".sizeof(char));
+	printf("Size of an int: %4 byte(s)".sizeof(int));
+	printf("Size of a long int: %4 byte(s)".sizeof(long int));
+	printf("Size of a long long int: %8 byte(s)".sizeof(long long int));
+	printf("Size of a float: %4 byte(s)".sizeof(float));
+	return (0);
+}
