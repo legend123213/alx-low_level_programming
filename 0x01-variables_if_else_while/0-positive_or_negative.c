46effe279893799f248e8a3680d660a8f9a76a32
Abel Wendmu <abelwendmu124@gmail.com>
2023-08-11 19:02:39 -0700

    hello there

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /* more headers goes there */
-
 /* betty style doc for function main goes there */ 
- 
 /**
  * main - Entry point
  *
