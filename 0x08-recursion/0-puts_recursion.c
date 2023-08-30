6e01f7574ca955887235aadb5b54f0858b706a91
root <abelwendmu124@gmail.com>
2023-08-30 10:16:25 -0700

    fixing betty

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * _puts_recursion - prints a string followed by new line
@@ -7,12 +7,13 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
-	_putchar('\n');
+	if(*s == '\0')
+	{
+		_putchar('\n');
+	}
 	else
 	{
-	_putchar(*s);
-	s++;
-	_puts_recursion(s);
+		_putchar(s[0]);
+		_puts_recursion(s + 1);
 	}
 }
