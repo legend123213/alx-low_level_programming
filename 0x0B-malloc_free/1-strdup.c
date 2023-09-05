104a3a0881b9f0392377a47c8538fad64b11c1a4
root <abelwendmu124@gmail.com>
2023-09-05 13:44:39 -0700

    fixing betty

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -5,32 +5,24 @@
  *space in memory, which contains a copy of the
  *string givn as a parameter.
  * @str:String to be copied
- * 
  * Return: NULL in case of error , pointer to allocated
  */
 
 char *_strdup(char *str)
 {
-    char *cpy;
-    int index, len;
-    if (str == NULL)
-        return (NULL);
+	char *cpy;
+	int index, len;
 
-    for (index = 0; str[index]; index++)
+	if (str == NULL)
+		return (NULL);
+	for (index = 0; str[index]; index++)
+		len++;
+	cpy = malloc(sizeof(char) * (len + 1));
+	if (cpy == NULL)
+		return (NULL);
+	for (index = 0; str[index]; index++)
+	{cpy[index] = str[index]; }
+	cpy[len] = '\0';
+	return (cpy);
 
-
-
-
-            len++;
-    cpy = malloc(sizeof(char) * (len + 1));
-
-    if (cpy == NULL)
-        return (NULL);
-
-    for (index = 0; str[index]; index++)
-    {
-        cpy[index] = str[index];
-    }
-    cpy[len] = '\0';
-    return (cpy);
 }
